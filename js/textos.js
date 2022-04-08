fe1fc088c011f5d395354a0b8b2e2b507f7aa723
aguspeiretti <aguspeiretti@gmail.com>
2022-04-08 18:40:40 -0300

    subida8/42

--- js/textos.js
+++ js/textos.js
@@ -55,7 +55,7 @@ const resp = new Typed('.resp', {
     strings: [" Agus <br> Peiretti. "],
     stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 65, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 3100, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	startDelay: 3200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
