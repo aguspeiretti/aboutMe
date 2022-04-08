bd408dfa5293858ac2582cdbb0694a3eb9914f7d
aguspeiretti <aguspeiretti@gmail.com>
2022-04-08 17:53:38 -0300

    primera subida

---
+++ js/swich.js
@@ -0,0 +1,8 @@
+const switchButton = document.getElementById('swich');
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+     
+    
+});
---
+++ js/textos.js
@@ -0,0 +1,87 @@
+const typed = new Typed('.typed', {
+    strings: ["Hi, <br> I'm Agus Peiretti."],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 65, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+
+});
+
+const typed1 = new Typed('.typed1', {
+    strings: ["Web <br> Developer"],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 65, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3550, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+
+});
+const primero = new Typed('.primero', {
+    strings: [" Hi! my name is: "],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 65, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+
+});
+
+const resp = new Typed('.resp', {
+    strings: [" Agustin Peiretti. "],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 95, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+
+});
+const tercero = new Typed('.tercero', {
+    strings: [" I'm a web developer!! "],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 65, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 5200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+
+});
